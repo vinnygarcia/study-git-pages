ac11cbaba4845d924cee79300bbed3e021fb5965
Vinicius Garcia <viniciusgarcia@id.uff.br>
2020-06-28 20:01:18 -0300

    adding a js script to the page

---
+++ main.js
@@ -0,0 +1,10 @@
+
+ window.onload = () => {
+    console.log("HELLO!!!");
+    // Creating a new h3 element
+    let h3 = document.createElement('h3');
+    h3.innerText = "MY new h3"
+    //Selecting the container div
+    let containerDiv = document.querySelector('#container');
+    containerDiv.appendChild(h3);
+ }
